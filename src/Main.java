2042cf7941970ceee687d9d1a9d0b44fc1d14c2a
GeorgioSav <GeorgioSav@yandex.ru>
2023-09-03 20:35:37 +0300

    ДЗ! Не понял как сделать 3 и 4 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+
+        //Task 1
+        System.out.println("Task 1");
+        int[] arr = {1, 2, 3};
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        int arr1 = arr[0];
+        int arr2 = arr[1];
+        int arr3 = arr[2];
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        double[] doubleArr = new double[4];
+        doubleArr[0] = 0;
+        doubleArr[1] = 1.57;
+        doubleArr[2] = 7.654;
+        doubleArr[3] = 9.986;
+        double arre0 = doubleArr[0];
+        double arre1 = doubleArr[1];
+        double arre2 = doubleArr[2];
+        double arre3 = doubleArr[3];
+        System.out.println(Arrays.toString(doubleArr));
+        System.out.println();
+
+        int[] fruits = new int[5];
+        fruits [0] = 100;
+        fruits [1] = 50;
+        fruits [2] = 10;
+        fruits [3] = 38;
+        fruits [4] = 15;
+        int lemonsFruits = fruits[0];
+        int applesFruits = fruits[1];
+        int orangesFruits = fruits[2];
+        int mangoFruits = fruits[3];
+        int passionFruitFruits = fruits[4];
+        System.out.println(Arrays.toString(fruits));
+        System.out.println();
+
+
+
 
     }
 }
