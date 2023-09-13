0540447d1af3eeac490744c370f9161f8d52c1dc
GeorgioSav <GeorgioSav@yandex.ru>
2023-09-13 22:35:43 +0300

    Домашняя работа задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //Task 4
+        System.out.println("Task 4");
+        int[] weight = new int[12];
+        weight[0] = 2;
+        weight[1] = 2;
+        weight[2] = 4;
+        for (int i = 0; i < 12; i++) {
+            System.out.print(weight[i] + " ");
+        }
+        System.out.println();
+
+
+
+
 
     }
 }
